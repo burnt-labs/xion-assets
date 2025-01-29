f4d524703f3e1016ea6bb774f5f463c5b544e2ea
2xburnt <169301814+2xburnt@users.noreply.github.com>
2025-01-29 12:07:05 -0600

    re-add modules

--- sources/sparse.sh
+++ sources/sparse.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-set -x
+set -euo pipefail
 
-for dir in $(find . -type d); do
+SOURCES_DIR=$(cd $(dirname $0) && pwd)
+
+cd $SOURCES_DIR
+for dir in $(ls -d */); do
+    dir=$(basename $dir)
+    echo "found dir: $dir"
     cd $dir
-    git config sparse-checkout true
-    echo "**xion**" .git/modules/sources/$dir/info/sparse-checkout
+    echo "setting sparse checkout on: $dir"
+    git config core.sparse-checkout true
+    find * -type f -name '*xion*' | sed 's/^.\.//' | tee ../../.git/modules/sources/$dir/info/sparse-checkout
+    git read-tree -mu HEAD
+    cd ..
 done
